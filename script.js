fb0cb65ddd55397026cb9f2566c21b0a00d9956e
JASPREET KAUR <94101161+jasbhatia@users.noreply.github.com>
2023-05-05 13:35:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	const map = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+	const result = [];
+	
+	if (digits.length === 0) {
+    return result;
+  }
+	const backtrack = (combination, nextDigits) => {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+    } else {
+      const letters = map[nextDigits[0]];
+      for (let i = 0; i < letters.length; i++) {
+        backtrack(combination + letters[i], nextDigits.slice(1));
+      }
+    }
+  };
+
+  backtrack('', digits);
+
+  return result.sort();
 }
 
+
 module.exports = letterCombinations;
+
+
+
+// module.exports = letterCombinations;
